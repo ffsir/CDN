89ca3c6694ff2f0613e51d9160b1ed57b2d26f57
ffsir <47908792+ffsir@users.noreply.github.com>
2021-01-24 14:45:28 +0800

    Update nodebug.js

--- nodebug.js
+++ nodebug.js
@@ -9,3 +9,51 @@
             return false;
         }
     };
+
+/**
+ * 检测到调试时进行的操作
+ */
+let onDebug = function () {
+    //提示
+    document.write('严禁调试！请关闭控制台，刷新恢复');
+    /*卡死*/
+   //  while (true) {
+    //     console.log('hi');
+    //    console.clear();
+     //}
+
+};
+
+setInterval(function () {
+    let st, et;
+    st = new Date().getTime();
+    debugger;
+    et = new Date().getTime();
+    if ((et - st) > 1000) {
+        onDebug();
+    }
+}, 1000);
+
+
+document.onkeydown = document.onkeyup = document.onkeypress = function (event) {
+    const e = event || window.event || arguments.callee.caller.arguments[0];
+
+    if (e && e.keyCode == 123) {
+        onDebug();
+    }
+};
+
+
+let div = document.createElement('div');
+Object.defineProperty(div, "id", {
+    get: () => {
+        clearInterval(loop);
+        onDebug();
+    }
+});
+let loop = setInterval(() => {
+    console.log(div);
+    console.clear();
+});
+
+
