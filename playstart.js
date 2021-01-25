a0cc86f55c983a7595edc0b1112e9aae11cfa9fe
ffsir <47908792+ffsir@users.noreply.github.com>
2021-01-25 16:46:34 +0800

    close

--- playstart.js
+++ playstart.js
@@ -1,4 +1,8 @@
-   $('#loading').hide();
+ 
+function close(){
+  $('#loading').hide();
     $('#player').remove();
     $('#error').show();
     $("#error").html('<h1>出于安全考虑：如果你是开发者请更新到最新版本，如果你是用户请尽快联系管理员更新！</h1>');
+}
+close();
