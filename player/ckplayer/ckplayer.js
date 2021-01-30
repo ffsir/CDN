f158b3c62790113550cd65a9bd6f958272c65c56
ffsir <47908792+ffsir@users.noreply.github.com>
2021-01-31 07:34:39 +0800

    update v2.0

--- player/ckplayer/ckplayer.js
+++ player/ckplayer/ckplayer.js
@@ -115,7 +115,7 @@
 		this.ckStyle = {};
 		this.jsonStyle = {};//该变量为一次性赋值，不再变化
 		//全局变量：右键菜单：[菜单标题,类型(link:链接，default:灰色，function：调用函数，javascript:调用js函数),执行内容(包含链接地址，函数名称),[line(间隔线)]]
-		
+		this.contextMenu = [['ckplayer', 'link', 'http://www.ckplayer.com', '_blank'], ['version:X2', 'default', 'line']];
 		//全局变量：错误提示列表
 		this.errorList = [
 			['000', 'Object does not exist'], 
@@ -141,7 +141,7 @@
 		//全局变量：HTML5当前显示的字幕编号
 		this.subtitlesTemp=-1;
 		//全局变量：定义logo
-		
+		this.logo = '';
 		//全局变量：是否加载了播放器
 		this.loaded = false;
 		//全局变量：计时器，监听视频加载出错的状态
